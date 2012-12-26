3e60cc71cde9b65a24dff6316fefa0a3e5cc3046
Mark McDowall <markus.mcd5@gmail.com>
2012-12-25 21:49:55 -0800

    Start of anime in services

---
+++ NzbDrone.Services/NzbDrone.Services.Service/Controllers/AnimeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using NzbDrone.Services.Service.Providers;
+
+namespace NzbDrone.Services.Service.Controllers
+{
+    public class AnimeController : Controller
+    {
+        private readonly AnimeProvider _animeProvider;
+
+        public AnimeController(AnimeProvider animeProvider)
+        {
+            _animeProvider = animeProvider;
+        }
+
+        [HttpGet]
+        [OutputCache(CacheProfile = "Cache1Hour")]
+        public JsonResult All()
+        {
+            var all = _animeProvider.All();
+
+            return Json(all, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [OutputCache(CacheProfile = "Cache1Hour")]
+        public JsonResult AllIds()
+        {
+            var all = _animeProvider.AllSeriesIds();
+
+            return Json(all, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [OutputCache(CacheProfile = "Cache1HourVaryBySeriesId")]
+        public JsonResult Check(int seriesId)
+        {
+            var result = _animeProvider.IsDaily(seriesId);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [OutputCache(CacheProfile = "Cache1HourVaryBySeriesId")]
+        public JsonResult AnidbId(int tvdbId)
+        {
+            
+        }
+    }
+}
---
+++ NzbDrone.Services/NzbDrone.Services.Service/Repository/Anime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Services.PetaPoco;
+
+
+namespace NzbDrone.Services.Service.Repository
+{
+    [TableName("Anime")]
+    [PrimaryKey("Id", autoIncrement = false)]
+    public class Anime
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int AnidbId { get; set; }
+    }
+}
